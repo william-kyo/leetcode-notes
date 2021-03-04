9e56adce14651ff8e341f03f48a07d36527931fe
xu.mingwei <xumingwei@deepexi.com>
2021-03-04 14:57:10 +0800

    binary search for kth smallest 2D matrix

--- src/array/KthSmallestElement.java
+++ src/array/KthSmallestElement.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import java.util.PriorityQueue;
 
 public class KthSmallestElement {
+    // 方式一：使用堆排序
+    /*
     public int kthSmallest(int[][] matrix, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>((x1, x2) -> x2.compareTo(x1));
         int n = matrix.length;
@@ -18,6 +20,35 @@ public class KthSmallestElement {
             pq.poll();
         }
         return pq.peek();
+    }
+    */
+
+    // 方式二： 使用二分查找
+    public int kthSmallest(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int lo = matrix[0][0];
+        int hi = matrix[m - 1][n - 1];
+
+        while (lo < hi) {
+            int count = 0;
+            int mid = lo + (hi - lo) / 2;
+
+            // 统计当前有多少个数比mid小
+            for (int i = 0; i < m; i++) {
+                int j = n - 1;
+                while (j >= 0 && matrix[i][j] > mid) {
+                    j--;
+                }
+                count += j + 1;
+            }
+            if (count < k) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
     }
 
     public static void main(String[] args) {
