d4155d6a019bbd875c99b8ebed28b5f0d8112e0d
xu.mingwei <huimingway@foxmail.com>
2021-07-24 09:18:00 +0800

    Search Range

---
+++ src/binary/search/FindRangeInSortedArray.java
@@ -0,0 +1,48 @@
+package binary.search;
+
+import org.junit.Assert;
+
+public class FindRangeInSortedArray {
+    public int[] searchRange(int[] nums, int target) {
+        if(nums==null||nums.length==0){
+            return new int[]{-1,-1};
+        }
+        int l = 0;
+        int h = nums.length;
+        while(l<h){
+            int mid = l + (h-l)/2;
+            if(nums[mid]<target){
+                l = mid+1;
+            } else {
+                h = mid;
+            }
+        }
+        if(l>=nums.length||nums[l]!=target){
+            return new int[]{-1,-1};
+        }
+        // find the last position
+        int last = l;
+        int p = l;
+        while(p<nums.length){
+            if(nums[p]==target){
+                last = p;
+            }
+            p++;
+        }
+        return new int[]{l, last};
+    }
+
+    public static void main(String[] args) {
+        int[] nums;
+        int target;
+        int[] expected, actual;
+        FindRangeInSortedArray fr = new FindRangeInSortedArray();
+
+        nums = new int[]{2,3,4,4,5};
+        target = 4;
+        expected = new int[]{2, 3};
+        actual = fr.searchRange(nums, target);
+        Assert.assertArrayEquals(expected, actual);
+
+    }
+}
